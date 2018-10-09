6cb4178a12afd2691cd3f02a5a630d4555f7f465
Chris McGee <chris@chrismcgee.info>
2018-10-09 14:36:09 -0400

    Success!
    However, the library files and type definitions are in temporary locations.
    
    Signed-off-by: Chris McGee <chris@chrismcgee.info>

--- dom_app/src/index.ts
+++ dom_app/src/index.ts
@@ -1,5 +1,3 @@
-/// <reference types="types-for-adobe/illustrator/2015.3"/>
-
 function openDocument(){
   $.writeln('in the openDocument() function.');
   var fileRef = new File("~/Product Templates (Master)/Art Evaluation Form/Art_Evaluation_Form.indd");
--- host/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/// <reference types="types-for-adobe/illustrator/2015.3"/>
-function openDocument() {
-    $.writeln('in the openDocument() function.');
-    var fileRef = new File("~/Product Templates (Master)/Art Evaluation Form/Art_Evaluation_Form.indd");
-    $.writeln(fileRef.fullName);
-    var docRef = app.open(fileRef);
-}
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUEsMkRBQTJEO0FBQzNELFNBQVMsWUFBWTtJQUNuQixDQUFDLENBQUMsT0FBTyxDQUFDLGlDQUFpQyxDQUFDLENBQUM7SUFDN0MsSUFBSSxPQUFPLEdBQUcsSUFBSSxJQUFJLENBQUMsMkVBQTJFLENBQUMsQ0FBQztJQUNwRyxDQUFDLENBQUMsT0FBTyxDQUFDLE9BQU8sQ0FBQyxRQUFRLENBQUMsQ0FBQztJQUU1QixJQUFJLE1BQU0sR0FBRyxHQUFHLENBQUMsSUFBSSxDQUFDLE9BQU8sQ0FBQyxDQUFDO0FBQ2pDLENBQUMifQ==
