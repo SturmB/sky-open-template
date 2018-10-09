6866ed317bae305c942039dd310c68d53a1c7e6d
Chris McGee <chris@chrismcgee.info>
2018-10-09 13:43:24 -0400

    Only need Host to work
    Signed-off-by: Chris McGee <chris@chrismcgee.info>

--- host/index.ts
+++ host/index.ts
@@ -1,4 +1,5 @@
 /// <reference types="types-for-adobe/illustrator/2015.3"/>
+
 function openDocument(){
   $.writeln('in the openDocument() function.');
   var fileRef = new File("~/Product Templates (Master)/Art Evaluation Form/Art_Evaluation_Form.indd");
